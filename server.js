ab5ba7d44a676a684f6f42c508f4c75c4e929293
Sarah Gambles <sarahgambles@Sarahs-MacBook-Pro.local>
2021-01-20 21:37:41 -0700

    refactored server to listen

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
+        console.log(`🌍 Connected on localhost:${PORT}!`);
     });
 });
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
